67810273fb4aa2b43286a54017dbc022527a8db8
fyenne <fyenne@hotmail.com>
2021-08-12 15:03:00 +0800

    message

--- kmeans_qty_wkhour/dws_dsc_wh_ou_daily_kpi_sum.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# pyspark python3 py2 
-export PYSPARK_PYTHON="/app/anaconda3/bin/python"
-export PYSPARK_DRIVER_PYTHON="/app/anaconda3/bin/python"
-spark-submit --master yarn --deploy-mode client --driver-memory 8G --executor-memory 8G --queue root.dsc ./dws_dsc_wh_ou_kpi_sum/dws_dsc_wh_ou_daily_kpi_sum.py --start_date ${start_date} --end_date ${end_date}
