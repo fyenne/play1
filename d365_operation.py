ddc377198beb4923098f4d83d5a6f93ad15295fc
fyenne <fyenne@hotmail.com>
2021-10-15 17:17:31 +0800

    add

--- d365_operation.py
+++ d365_operation.py
@@ -30,6 +30,32 @@ df = spark.sql("""select *
 
 df = df.select("*").toPandas()
 
+
+
+df2 =  spark.sql("""select substr(inc_day, 1,6) as month, ou_code
+        ,count(w.emp_code) as total_head_count
+        ,count(distinct case when lower(w.emp_type) = 'perm' then w.emp_code else '' end) - 1 as pm_head_cnt
+        ,count(distinct case when lower(w.emp_type) = 'outsource' then w.emp_code else '' end) - 1 as os_head_cnt
+        ,count(distinct case 
+        when lower(w.emp_type) = 'contract' then w.emp_code
+        when lower(w.emp_type) = 'sub contract' then w.emp_code
+        else '' end) - 1 as ctrct_head_cnt
+        ,sum(w.working_hours) as total_working_hours
+        ,sum(case when lower(w.emp_type) = 'perm' then w.working_hours else 0 end) as perm_working_hours
+        ,sum(case when lower(w.emp_type) = 'outsource' then w.working_hours else 0 end) as outsource_working_hours
+        ,sum(case when lower(w.emp_type) not in ('perm','outsource') or w.emp_type is null then w.working_hours else 0 end) as other_working_hour
+        ,sum(case when lower(s.direct) = 'direct' then w.working_hours else  0 end) as direct_working_hour
+        ,sum(case when lower(coalesce(s.direct,'indirect')) = 'indirect' then w.working_hours else 0 end) as indirect_working_hour
+        from dsc_dwd.dwd_hr_dsc_working_hour_dtl_di w
+        left join dsc_dim.dim_dsc_staff_info s
+        on w.emp_code = s.staff_id
+        where ou_code <> ''
+        and working_hours > 0 
+        group by ou_code, substr(inc_day, 1,6) """)
+
+df2 = df2.select("*").toPandas()
+
+
 """
 test local
 """
@@ -59,371 +85,17 @@ inb oub qty sum always nill,  will be removed.
 only keep rows where total working hour is not nill 
 """
 # clean_df0 :
+df = df.dropna(axis =0,subset =['ou_code'])
 df['operation_day'] = df['operation_day'].apply(int)
-df = df[df['operation_day'] >= 20210601] 
+# df = df[df['operation_day'] >= 20210601] 
 
 
-# clean_df1 = (df.groupby('ou_code')['operation_day'].count() < 2).reset_index()
-# clean_df1.columns = ['ou_code', 'flag1']
-# df = clean_df1.merge(df, on = 'ou_code', how = 'inner')
-# df = df[df['flag1'] == False]
-
-clean_df2 = df.groupby('ou_code')[[
-    'inbound_receive_qty', 'outbound_shipped_qty'
-    ]].sum().reset_index()
-clean_df2['sum'] = clean_df2.sum(axis = 1)
-clean_df2 = clean_df2[clean_df2['sum'] != 0]
-df = df[df['ou_code'].isin(clean_df2.ou_code)]
-
-clean_df3 = (df.groupby('ou_code')[[
-    'total_working_hour'
-    ]].sum() == 0).reset_index()
-clean_df3 = clean_df3[clean_df3['total_working_hour'] == False]
-
- 
-df = df[df['ou_code'].isin(clean_df3.ou_code)]
-df= df.reset_index()
-
-df = df[[
-    'ou_code','operation_day', 'inbound_receive_qty', 'is_holiday',
-    'outbound_shipped_qty','total_head_count','total_working_hour',
-    'outsource_working_hour', 'perm_working_hour',
-    'other_working_hour', 'direct_working_hour', 'indirect_working_hour',
-    'outbound_inbound_qty_ratio', 'perm_working_hour_ratio',
-    'working_hour_per_head', 'location_usage_rate', 'location_idle_rate']]
-df = df.fillna(0)
-df = df[df['total_working_hour'] != 0]
- 
-view = df[df['ou_code'] == 'CN-066'].sort_values('operation_day')
-view['operation_day'].head(40)
-
-"""
-calculations functions def automate
-""" 
-# # from sklearn.metrics import davies_bouldin_scores
-# daylen = int(df['operation_day'].nunique()/10)
-# def mnb_kmeans_in(ou_code):
-#         """
-#         mini batch kmeans, inbound, outbound, working hour data.
-#         simple algorithm, adding cols {max, min, mean, median, 75 quantile, distance to kernal}
-#         Calculate Davies Bouldin score
-#         from sklearn.metrics import davies_bouldin_score
-#         null data fill
-#         """
-
-#         df_fin = pd.DataFrame()
-#         df_sub = df[df['ou_code'] == ou_code][['ou_code', 'operation_day', 'inbound_receive_qty']]        
-#         df_fin = df_fin.append(df_sub[df_sub['inbound_receive_qty'] == 0])
-#         df_fin['kernal_core1' ] = -1
-#         df_fin['kernal_value1'] = 0
-#         df_rec = df_sub[df_sub['inbound_receive_qty'] != 0]      
-
-#         """
-#         auto choose kernel nums
-#         """
-#         scores = []
-#         for center in list(range(2,daylen)):
-#             kmeans = cluster.MiniBatchKMeans(n_clusters=center)
-#             model  = kmeans.fit_predict(
-#                         X = np.reshape(list(df_rec['inbound_receive_qty']), (-1,1))
-#                         )
-#             score  = davies_bouldin_score(
-#                         np.reshape(list(df_rec['inbound_receive_qty']), (-1,1)), model
-#                         )
-#             scores.append(score)
-
-#         center = np.argmin(scores)
-#         print(center)
-     
-#         """
-#         not null data training
-#         """
-    
-#         alg1 = cluster.MiniBatchKMeans(n_clusters = center, random_state = 5290403)
-#         hist1 = alg1.fit(np.reshape(list(df_rec['inbound_receive_qty']), (-1,1)))
-#         df_rec['kernal_core1'] = hist1.labels_
-#         cl_1 = pd.concat(
-#                 [pd.DataFrame(hist1.cluster_centers_), pd.Series(np.arange(0,center))], axis = 1
-#                 )
-        
-#         cl_1.columns = ['kernal_value1', 'kernal_core1']
-#         df_rec = df_rec.merge(
-#                 cl_1, on = 'kernal_core1', how = 'inner'
-#                 )
-        
-#         """
-#         merging
-#         """
-#         df_fin = df_fin.append(df_rec).reset_index().drop(['index'], axis = 1)
-
-#         # df_fin['kind'] = 'inbound'
-
-#         return df_fin
+df['qty_sum'] = df['inbound_receive_qty']+ df['outbound_shipped_qty']
+df['montly_qty_sum'] = df.groupby(
+    ['month', 'ou_code'])['qty_sum'].transform('sum')
 
 
-
-# def mnb_kmeans_out(ou_code):
-        
-#         df_fin = pd.DataFrame()
-#         df_sub = df[df['ou_code'] == ou_code][['ou_code', 'operation_day', 'outbound_shipped_qty']]        
-#         df_fin = df_fin.append(df_sub[df_sub['outbound_shipped_qty'] == 0])
-#         df_fin['kernal_core2' ] = -1
-#         df_fin['kernal_value2'] = 0
-#         df_rec = df_sub[df_sub['outbound_shipped_qty'] != 0]
-
-#         scores = []
-#         for center in list(range(2,daylen)):
-#             kmeans = cluster.MiniBatchKMeans(n_clusters=center)
-#             model  = kmeans.fit_predict(
-#                         X = np.reshape(list(df_rec['outbound_shipped_qty']), (-1,1))
-#                         )
-#             score  = davies_bouldin_score(
-#                         np.reshape(list(df_rec['outbound_shipped_qty']), (-1,1)), model
-#                         )
-#             scores.append(score)
-
-#         center = np.argmin(scores)
-#         print(center)
-#         alg1 = cluster.MiniBatchKMeans(n_clusters = center, random_state = 5290403)
-#         hist1 = alg1.fit(np.reshape(list(df_rec['outbound_shipped_qty']), (-1,1)))
-
-#         df_rec['kernal_core2'] = hist1.labels_
-#         cl_1 = pd.concat(
-#                 [pd.DataFrame(hist1.cluster_centers_), pd.Series(np.arange(0,center))], axis = 1
-#                 )
-        
-#         cl_1.columns = ['kernal_value2', 'kernal_core2']
-
-#         df_rec = df_rec.merge(
-#                 cl_1, on = 'kernal_core2', how = 'inner'
-#                 )
-#         df_fin = df_fin.append(df_rec).reset_index().drop(['index'], axis = 1)
-#         # df_fin['kind'] = 'outbound'
-#         return df_fin
-
- 
-"""
-calculations functions def 0
-""" 
-nc = 7
-def mnb_kmeans_in(ou_code):
-        """
-        mini batch kmeans, inbound, outbound, working hour data.
-        simple algorithm, adding cols {max, min, mean, median, 75 quantile, distance to kernal}
-        """
-        
-        alg1 = cluster.MiniBatchKMeans(n_clusters = nc, random_state = 5290403)
-        """
-        null data fill
-        """
-        df_fin = pd.DataFrame()
-        df_sub = df[df['ou_code'] == ou_code][['ou_code', 'operation_day', 'inbound_receive_qty']]        
-        df_fin = df_fin.append(df_sub[df_sub['inbound_receive_qty'] == 0])
-        df_fin['kernal_core1' ] = -1
-        df_fin['kernal_value1'] = 0
-        """
-        not null data training
-        """
-        df_rec = df_sub[df_sub['inbound_receive_qty'] != 0]      
-        hist1 = alg1.fit(np.reshape(list(df_rec['inbound_receive_qty']), (-1,1)))
-        df_rec['kernal_core1'] = hist1.labels_
-        cl_1 = pd.concat(
-                [pd.DataFrame(hist1.cluster_centers_), pd.Series(np.arange(0,nc))], axis = 1
-                )
-        
-        cl_1.columns = ['kernal_value1', 'kernal_core1']
-        df_rec = df_rec.merge(
-                cl_1, on = 'kernal_core1', how = 'inner'
-                )
-        
-        """
-        merging
-        """
-        df_fin = df_fin.append(df_rec).reset_index().drop(['index'], axis = 1)
-
-        # df_fin['kind'] = 'inbound'
-
-        return df_fin
-
-
-def mnb_kmeans_out(ou_code):
-        alg1 = cluster.MiniBatchKMeans(n_clusters = nc, random_state = 5290403)
-        df_fin = pd.DataFrame()
-        df_sub = df[df['ou_code'] == ou_code][['ou_code', 'operation_day', 'outbound_shipped_qty']]        
-        df_fin = df_fin.append(df_sub[df_sub['outbound_shipped_qty'] == 0])
-        df_fin['kernal_core2' ] = -1
-        df_fin['kernal_value2'] = 0
-        df_rec = df_sub[df_sub['outbound_shipped_qty'] != 0]
-
-        hist1 = alg1.fit(np.reshape(list(df_rec['outbound_shipped_qty']), (-1,1)))
-
-        df_rec['kernal_core2'] = hist1.labels_
-        cl_1 = pd.concat(
-                [pd.DataFrame(hist1.cluster_centers_), pd.Series(np.arange(0,nc))], axis = 1
-                )
-        
-        cl_1.columns = ['kernal_value2', 'kernal_core2']
-
-        df_rec = df_rec.merge(
-                cl_1, on = 'kernal_core2', how = 'inner'
-                )
-        df_fin = df_fin.append(df_rec).reset_index().drop(['index'], axis = 1)
-        # df_fin['kind'] = 'outbound'
-        return df_fin
-
-"""
-calculations functions def 0
-""" 
-
-def mnb_kmeans_hr(ou_code):
-        alg1 = cluster.MiniBatchKMeans(n_clusters = nc, random_state = 5290403)
-        df_fin = pd.DataFrame()
-        df_sub = df[df['ou_code'] == ou_code][['ou_code', 'operation_day', 'total_working_hour']]        
-        df_fin = df_fin.append(df_sub[df_sub['total_working_hour'] == 0])
-        df_fin['kernal_core3' ] = -1
-        df_fin['kernal_value3'] = 0
-        df_rec = df_sub[df_sub['total_working_hour'] != 0]
-
-        hist1 = alg1.fit(np.reshape(list(df_rec['total_working_hour']), (-1,1)))
-
-        df_rec['kernal_core3'] = hist1.labels_
-        cl_1 = pd.concat(
-                [pd.DataFrame(hist1.cluster_centers_), pd.Series(np.arange(0,nc))], axis = 1
-                )
-        
-        cl_1.columns = ['kernal_value3', 'kernal_core3']
-
-        df_rec = df_rec.merge(
-                cl_1, on = 'kernal_core3', how = 'inner'
-                )
-        df_fin = df_fin.append(df_rec).reset_index().drop(['index'], axis = 1)
-
-        # df_fin['kind'] = 'working_hour'
-        # df_fin['max_wh']    = df_fin.groupby('kernal_core3')['total_working_hour'].transform('max')
-        # df_fin['min_wh']    = df_fin.groupby('kernal_core3')['total_working_hour'].transform('min')
-        # df_fin['median_wh'] = df_fin.groupby('kernal_core3')['total_working_hour'].transform('median')
-        # df_fin['mean_wh']   = df_fin.groupby('kernal_core3')['total_working_hour'].transform('mean')
-        # df_fin['qt_66_wh']  = df_fin.groupby('kernal_core3')['total_working_hour'].transform('quantile', .66)
-        # df_fin['qt_75_wh']  = df_fin.groupby('kernal_core3')['total_working_hour'].transform('quantile', .75)
-        """
-        组内kernal distance 
-        """
-        df_fin['dis_core']  = df_fin.groupby(
-                'kernal_core3'
-                )['total_working_hour','kernal_value3'].diff(
-                    axis = 1
-                    ).drop('total_working_hour', axis = 1).round(3)
-        return df_fin
-
-
-"""
-remove OUs which can not be calculate due to deficiency of data length.
-"""
-
-ou_codes = list(df['ou_code'].unique())
-p = list()
-for i in ou_codes:
-    try: 
-        mnb_kmeans_in(i)
-        mnb_kmeans_out(i)
-        mnb_kmeans_hr(i)
-    except:
-        p.append(i)
-
-
-for i in p:
-    ou_codes.remove(i)
-
-print(p)
-ou_codes
-"""
-for loop , 对所有ou进行独立的kmeans on inb qty and outb qty
-随后merge 原始表
-and other calculation measures.
-"""
-
-from functools import reduce
-df_final = pd.DataFrame()
-for i in ou_codes:
-        df_final = df_final.append(
-            reduce(
-                lambda left,right: pd.merge(
-                    left,right,on= ['ou_code', 'operation_day']
-                ), [mnb_kmeans_in(i), mnb_kmeans_out(i), mnb_kmeans_hr(i)]
-                )
-    )
-
-df_final.tail()
-
-# np.setdiff1d(ou_codes,df_final.ou_code.unique())
-df_final = df_final.merge(
-    df[['ou_code','operation_day','outbound_inbound_qty_ratio','working_hour_per_head','total_head_count','is_holiday']],
-    on = ['ou_code', 'operation_day'],
-    how = 'left'
-    )
-print("=================================calculate_1================================")
- 
-view = df_final[df_final['ou_code'] == 'CN-066'].sort_values('operation_day')
-view['operation_day'].head(40)
-
-"""
-add outsource part
-"""
-def mnb_kmeans_hr2(ou_code):
-        alg1 = cluster.MiniBatchKMeans(n_clusters = nc, random_state = 5290403)
-        df_fin = pd.DataFrame()
-        df_sub = df[df['ou_code'] == ou_code][['ou_code', 'operation_day', 'outsource_working_hour']]  
-        df_fin = df_fin.append(df_sub[df_sub['outsource_working_hour'] == 0])
-        df_fin['kernal_core4' ] = -1
-        df_fin['kernal_value4'] = 0
-        df_rec = df_sub[df_sub['outsource_working_hour'] != 0]
- 
-        hist1 = alg1.fit(np.reshape(list(df_rec['outsource_working_hour']), (-1,1)))
-
-        df_rec['kernal_core4'] = hist1.labels_
-        cl_1 = pd.concat(
-                [pd.DataFrame(hist1.cluster_centers_), pd.Series(np.arange(0,nc))], axis = 1
-                )
-        
-        cl_1.columns = ['kernal_value4', 'kernal_core4']
-
-        df_rec = df_rec.merge(
-                cl_1, on = 'kernal_core4', how = 'outer'
-                )
-        df_fin = df_fin.append(df_rec).reset_index().drop(['index'], axis = 1)
-        df_fin['dis_core_os']=df_fin.groupby(
-                'kernal_core4'
-                )['outsource_working_hour','kernal_value4'].diff(
-                        axis = 1
-                        ).drop('outsource_working_hour', axis = 1).round(3)
-        return df_fin
-
-
-ou_codes
-p = list()
-for i in ou_codes:
-    try: 
-        # mnb_kmeans_in(i)
-        # mnb_kmeans_out(i)
-        # mnb_kmeans_hr(i)
-        mnb_kmeans_hr2(i)
-    except:
-        
-        p.append(i)
-
-
-for i in p:
-    ou_codes.remove(i)
- 
-
-df_final2 = pd.DataFrame()
-for i in ou_codes:
-        df_final2 = df_final2.append(
-            mnb_kmeans_hr2(i)
-        )
-
-df_final = df_final.merge(df_final2, on =  ['ou_code', 'operation_day'], how = 'left').fillna(0)
-
+  
 
 print("===================calculate_2, below is other calculate measures=========================")
 """
@@ -523,44 +195,11 @@ df_final['qt_75_dis_core_os_outer'] = df_final.groupby(
 """
 工时异常以及额外工时标记.
 """
-
-df_final['flag_75_os'] =  df_final['outsource_working_hour'] -1.2*df_final['qt_75_os']
-df_final['flag_75_os'] = [1 if a > 0 else 0 for a in df_final['flag_75_os']]
-
-
-df_final['flag_75_wh'] =  df_final['total_working_hour'] -1.2*df_final['qt_75_wh']
-df_final['flag_75_wh'] = [1 if a > 0 else 0 for a in df_final['flag_75_wh']]
-# df_final['flag_75_wh'] = [1 if df_final['total_working_hour'][i]\
-#     -1.2*np.abs(df_final['qt_75_wh'][i]) > 0
-#     else 0 for i in np.arange(0, len(df_final))]
-# df_final['flag_75_wh'] = [1 if df_final['dis_core_os'][i]>np.abs(df_final['qt_75_dis_core_os_outer'][i])\
-#      else 0 for i in np.arange(0, len(df_final))]
+ 
 
 print("=================================1================================")
-
-df_final  = df_final.replace(float('inf'), 0) 
-
-
-
-df_final0 = df_final[df_final['flag_75_wh'] == 0] 
-df_final1 = df_final[df_final['flag_75_wh'] == 1] 
-df_final0['dis_tt_kernel'] = 0
-df_final1['dis_tt_kernel'] = np.abs(df_final1['total_working_hour']  - 1.2* df_final1['qt_75_wh'] )
-
-
-df_final = pd.concat([df_final0, df_final1], axis = 0)
-
-print("1.2 is the final flag boundary")
-
-df_final0 = df_final[df_final['flag_75_os'] == 0] 
-df_final1 = df_final[df_final['flag_75_os'] == 1] 
-df_final0['dis_os_kernel'] = 0
-df_final1['dis_os_kernel'] = np.abs(df_final1['outsource_working_hour']  - 1.2* df_final1['qt_75_os'] )
-
-
-df_final = pd.concat([df_final0, df_final1], axis = 0)
-
-print("1.2 is the os flag boundary")
+  
+ 
 
 # diff_tt_kn = pd.concat([diff_tt_kn.rename({'qt_75_os' : 'dis_tt_kernel'}, axis = 1), \
 #     df_final[df_final['flag_75_wh'] == 1]], axis = 1)[['dis_tt_kernel', 'ou_code', 'operation_day']]
